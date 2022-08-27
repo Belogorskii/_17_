e2d3aaf567789481a7a511db43d1c2c29e8d6b49
Kirill Miller <belogorski2000@gmail.com>
2022-08-27 13:35:36 +0300

    Добавьте файлы проекта.

---
+++ _17_/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17
+{
+    class Employee<T>
+    {
+        public T Num { get; set; }
+        public string Name { get; set; }
+        public int Balance { get; set; }
+        //public Employee(T num, string name, int summa)
+        //{
+        //    Num = num;
+        //    Name = name;
+        //    Balance = balance;
+        //}
+        public string GoInfo()
+        {
+            return $"{Num} {Name} {Balance}";
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            Num = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите ФИО владельца счета");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите желаемый баланс :=) ");
+            Balance = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ _17_/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Создать класс для моделирования счета в банке. Предусмотреть закрытые поля для номера счета, 
+  баланса, ФИО владельца. Класс должен быть объявлен как обобщенный. Универсальный параметр T 
+  должен определять тип номера счета. Разработать методы  для  доступа  к  данным  –  заполнения  
+  и чтения. Создать  несколько экземпляров класса, параметризированного различными типам. Заполнить 
+  его поля и вывести информацию об экземпляре класса на печать.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите числовой номер");
+            //int numInt = Convert.ToInt32(Console.ReadLine());        
+            //Console.WriteLine("Введите имя");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Введите зп");
+            //int summa = Convert.ToInt32(Console.ReadLine());
+            //Employee<int> employee1 = new Employee<int>(numInt, name, summa);
+            //Console.WriteLine(employee1.GoInfo());
+
+            //Console.WriteLine("Введите числовой номер");
+            //string numString = Console.ReadLine();
+            //Console.WriteLine("Введите имя");
+            //name = Console.ReadLine();
+            //Console.WriteLine("Введите зп");
+            //summa = Convert.ToInt32(Console.ReadLine());
+            //Employee<string> employee2 = new Employee<string>(numString, name, summa);
+            //Console.WriteLine(employee2.GoInfo());
+
+            Employee<string> employee1 = new Employee<string>();
+            employee1.Input();
+            Console.WriteLine(employee1.GoInfo());
+            Employee<string> employee2 = new Employee<string>();
+            employee2.Input();
+            Console.WriteLine(employee2.GoInfo());
+            Console.ReadKey();
+        }
+    }
+}
